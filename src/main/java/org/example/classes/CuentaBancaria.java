1fd95590b7520decd298d1615aff6ea8cc9145ed
Jeboy <tovilajesus@gmail.com>
2023-03-24 19:13:52 -0300

    Second commit

---
+++ src/main/java/org/example/classes/CuentaBancaria.java
@@ -0,0 +1,26 @@
+package org.example.classes;
+
+public class CuentaBancaria {
+
+    private Usuario titular;
+
+    private double saldo;
+
+    public CuentaBancaria(Usuario usuario){
+        titular = usuario;
+    }
+
+    public Usuario getUsuario(){
+        return titular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+    public void depositar(double saldoADepositar){
+        if (saldoADepositar <=0){
+            throw new IllegalArgumentException("La cantidad a depositar debe ser mayor a cero");
+        }
+        saldo += saldoADepositar;
+    }
+}
