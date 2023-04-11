d1a3b22a465584f584e442e6fb3b1a91a2d02457
Jeboy <tovilajesus@gmail.com>
2023-04-10 21:12:49 -0300

    Se actualizó el archivo CuentaBancaria

--- src/test/java/org/example/classes/CuentaBancariaTest.java
+++ src/test/java/org/example/classes/CuentaBancariaTest.java
@@ -1,4 +1,58 @@
 package org.example.classes;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CuentaBancariaTest {
+
+    private CuentaBancaria cuenta;
+
+    private Usuario titular;
+
+    private Usuario titular2;
+
+    private final double SALDO_AGREGADO = 500;
+
+    @Before
+    public void setUp(){
+        titular = new Usuario();
+        titular2 = new Usuario("Cristian", "Gadea");
+        titular.setNombre("Carlos");
+        titular.setApellido("Páez");
+        cuenta = new CuentaBancaria(titular);
+    }
+
+    @Test
+    public void testGetUsuario(){
+        assertEquals(titular, cuenta.getUsuario());
+    }
+
+    @Test
+    public void testSetUsuario(){
+        cuenta.setUsuario(titular2);
+        assertEquals(titular2,cuenta.getUsuario());
+    }
+
+    @Test
+    public void testSetSaldo(){
+        cuenta.setSaldo(SALDO_AGREGADO);
+        assertEquals(SALDO_AGREGADO, cuenta.getSaldo(), 0.01);
+    }
+
+    @Test
+    public void testDepositar(){
+        cuenta.depositar(500.0);
+        assertEquals(500.0, cuenta.getSaldo(), 0.01);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDepositarCantidadNegativa(){
+        cuenta.depositar(-500.0);
+    }
+
+
+
+
 }
